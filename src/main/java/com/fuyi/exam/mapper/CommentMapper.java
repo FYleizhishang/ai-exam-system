0706fe70e013756bb89202c301e3b1763228cd83
付艺 <2105624742@qq.com>
2026-02-16 17:21:18 +0800

    Revert "第十五次提交：完成了该项目的所有功能"
    This reverts commit cd8358951c256964c7599309159e822ac39ce7a6.

--- src/main/java/com/fuyi/exam/mapper/CommentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.fuyi.exam.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.fuyi.exam.entity.Comment;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CommentMapper extends BaseMapper<Comment> {
-}
